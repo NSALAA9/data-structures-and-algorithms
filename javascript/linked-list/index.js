1ba1f4577782a53aa7d60dd90d6cefa0a435c61c
NSALAA9 <alaansairat99@gmail.com>
2023-06-21 23:39:32 +0000

    LINKED LIST

--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,44 @@
 'use strict';
 
-class LinkedList {
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+}
+}
 
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+  }
+
+  includes(value) {
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      if (currentNode.value === value) {
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+    return false;
+  }
+
+  toString() {
+    let currentNode = this.head;
+    let result = '';
+    while (currentNode !== null) {
+      result += `{ ${currentNode.value} } -> `;
+      currentNode = currentNode.next;
+    }
+    result += 'NULL';
+    return result;
+  }
 }
 
 module.exports = LinkedList;
