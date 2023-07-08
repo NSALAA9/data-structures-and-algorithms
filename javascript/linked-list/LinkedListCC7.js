bb792ef66d8bc001ebb68b4f091057126d13e0ed
NSALAA9 <alaansairat99@gmail.com>
2023-07-08 23:39:42 +0000

    CC7 linked-list-kth

--- javascript/linked-list/LinkedListCC7.js
+++ javascript/linked-list/LinkedListCC7.js
@@ -1,4 +1,51 @@
-'use strict';
+// 'use strict';
+// class Node {
+//   constructor(value) {
+//     this.value = value;
+//     this.next = null;
+//   }
+// }
+
+
+// class LinkedList {
+//   constructor() {
+//     this.head = null;
+//   }
+
+//   kthFromEnd(k) {
+//     if (!this.head) {
+//       throw new Error('Empty linked list');
+//     }
+
+//     if (k <= 0) {
+//       throw new Error('k must be a positive integer');
+//     }
+
+//     let p1 = this.head;
+//     let p2 = this.head;
+
+//     // Move p2 k nodes ahead
+//     for (let i = 0; i < k; i++) {
+//       if (!p2) {
+//         throw new Error('k is greater than the length of the linked list');
+//       }
+//       p2 = p2.next;
+//     }
+
+//     // Move both pointers until p2 reaches the end
+//     while (p2 && p2.next) {
+//       p1 = p1.next;
+//       p2 = p2.next;
+//     }
+
+//     return p1.value;
+//   }
+// }
+// const list = new LinkedList();
+// const node= new Node();
+
+// module.exports = LinkedList ;
+
 class Node {
   constructor(value) {
     this.value = value;
@@ -11,36 +58,52 @@ class LinkedList {
     this.head = null;
   }
 
-  kthFromEnd(k) {
+  append(value) {
+    const newNode = new Node(value);
+
     if (!this.head) {
-      throw new Error('Empty linked list');
-    }
-
-    if (k <= 0) {
-      throw new Error('k must be a positive integer');
-    }
-
-    let p1 = this.head;
-    let p2 = this.head;
-
-    // Move p2 k nodes ahead
-    for (let i = 0; i < k; i++) {
-      if (!p2) {
-        throw new Error('k is greater than the length of the linked list');
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
       }
-      p2 = p2.next;
+      current.next = newNode;
+    }
+  }
+
+  kthFromEnd(k) {
+    if (k < 0) {
+      throw new Error('k must be a non-negative integer.');
     }
 
-    // Move both pointers until p2 reaches the end
-    while (p2 && p2.next) {
-      p1 = p1.next;
-      p2 = p2.next;
+    if (!this.head) {
+      throw new Error('Cannot perform operation on an empty list.');
     }
 
-    return p1.value;
+    let slow = this.head;
+    let fast = this.head;
+
+    for (let i = 0; i < k; i++) {
+      if (!fast.next) {
+        throw new Error('k is greater than the length of the linked list.');
+      }
+      fast = fast.next;
+    }
+
+    while (fast.next) {
+      slow = slow.next;
+      fast = fast.next;
+    }
+
+    return slow.value;
   }
 }
-const list = new LinkedList();
-const node= new Node();
 
-module.exports = LinkedList ;
+// const list = new LinkedList();
+// const node= new Node();
+
+
+module.exports = { LinkedList, Node };
+
+
--- javascript/linked-list/__tests__/LinkedListCC7.test.js
+++ javascript/linked-list/__tests__/LinkedListCC7.test.js
@@ -1,41 +1,77 @@
-const { LinkedList, Node } = require("../LinkedListCC7");
+const { LinkedList, Node } = require('../LinkedListCC7');
 
-describe("LinkedList", () => {
-  let ll;
+// describe("LinkedList", () => {
+//   let ll;
 
-  beforeEach(() => {
-    ll = new LinkedList();
-    ll.head = new Node(1);
-    ll.head.next = new Node(3);
-    ll.head.next.next = new Node(8);
-    ll.head.next.next.next = new Node(2);
-  });
-  });
+//   beforeEach(() => {
+//     ll = new LinkedList();
+//     ll.head = new Node(1);
+//     ll.head.next = new Node(3);
+//     ll.head.next.next = new Node(8);
+//     ll.head.next.next.next = new Node(2);
+//   });
+//   });
 
-  it("should return the node's value that is k places from the tail of the linked list", () => {
-    assert.strictEqual(ll.kthFromEnd(0), 2);
-    assert.strictEqual(ll.kthFromEnd(2), 3);
-  });
+//   it("should return the node's value that is k places from the tail of the linked list", () => {
+//     assert.strictEqual(ll.kthFromEnd(0), 2);
+//     assert.strictEqual(ll.kthFromEnd(2), 3);
+//   });
 
-  it("should throw an exception when k is greater than the length of the linked list", () => {
-    assert.throws(() => {
-      ll.kthFromEnd(6);
-    }, Error);
-  });
+//   it("should throw an exception when k is greater than the length of the linked list", () => {
+//     assert.throws(() => {
+//       ll.kthFromEnd(6);
+//     }, Error);
+//   });
 
-  it("should throw an exception when k is not a positive integer", () => {
-    assert.throws(() => {
-      ll.kthFromEnd(-1);
-    }, Error);
-  });
+//   it("should throw an exception when k is not a positive integer", () => {
+//     assert.throws(() => {
+//       ll.kthFromEnd(-1);
+//     }, Error);
+//   });
 
-  it("should return the node's value when the linked list is of size 1", () => {
-    const singleNodeLL = new LinkedList();
-    singleNodeLL.head = new Node(5);
-    assert.strictEqual(singleNodeLL.kthFromEnd(0), 5);
-  });
+//   it("should return the node's value when the linked list is of size 1", () => {
+//     const singleNodeLL = new LinkedList();
+//     singleNodeLL.head = new Node(5);
+//     assert.strictEqual(singleNodeLL.kthFromEnd(0), 5);
+//   });
 
-  it("should return the node's value when k is in the middle of the linked list", () => {
-    assert.strictEqual(ll.kthFromEnd(1), 8);
-  });
+//   it("should return the node's value when k is in the middle of the linked list", () => {
+//     assert.strictEqual(ll.kthFromEnd(1), 8);
+//   });
+// });
+
+test('kthFromEnd should return the correct node value', () => {
+  // Create a linked list
+  const ll = new LinkedList();
+  ll.append(1);
+  ll.append(3);
+  ll.append(8);
+  ll.append(2);
+
+  // Test case: k = 0, expected output: 2
+  expect(ll.kthFromEnd(0)).toBe(2);
+
+  // Test case: k = 2, expected output: 3
+  expect(ll.kthFromEnd(2)).toBe(3);
+
+  // Test case: k = 6 (greater than list length), expected output: Error
+  expect(() => ll.kthFromEnd(6)).toThrow('k is greater than the length of the linked list.');
+
+  // Test case: k = -1 (negative value), expected output: Error
+  expect(() => ll.kthFromEnd(-1)).toThrow('k must be a non-negative integer.');
+
+  // Test case: Empty list, expected output: Error
+  const emptyList = new LinkedList();
+  expect(() => emptyList.kthFromEnd(1)).toThrow('Cannot perform operation on an empty list.');
+
+  // Test case: List with only one node, k = 0, expected output: 1
+  const singleNodeList = new LinkedList();
+  singleNodeList.append(1);
+  expect(singleNodeList.kthFromEnd(0)).toBe(1);
+
+  // Test case: List with only one node, k = 1 (greater than list length), expected output: Error
+  expect(() => singleNodeList.kthFromEnd(1)).toThrow('k is greater than the length of the linked list.');
+
+  // Test case: List with multiple nodes, k = 1 (middle node), expected output: 8
+  expect(ll.kthFromEnd(1)).toBe(8);
 });
