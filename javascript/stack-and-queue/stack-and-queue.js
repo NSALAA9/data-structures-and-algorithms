ede3fc434a4e0a6e8f1c1a913996d7af488d7710
NSALAA9 <alaansairat99@gmail.com>
2023-07-22 19:17:32 +0000

    updates

--- javascript/stack-and-queue/stack-and-queue.js
+++ javascript/stack-and-queue/stack-and-queue.js
@@ -1,9 +1,89 @@
+// class Node {
+//   constructor(value) {
+//     this.value = value;
+//     this.next = null;
+//   }
+// }
+// class Stack {
+//   constructor() {
+//     this.top = null;
+//   }
+
+//   push(value) {
+//     const newNode = new Node(value);
+//     newNode.next = this.top;
+//     this.top = newNode;
+//   }
+
+//   pop() {
+//     if (this.isEmpty()) {
+//       throw new Error('Stack is empty');
+//     }
+//     const value = this.top.value;
+//     this.top = this.top.next;
+//     return value;
+//   }
+
+//   peek() {
+//     if (this.isEmpty()) {
+//       throw new Error('Stack is empty');
+//     }
+//     return this.top.value;
+//   }
+
+//   isEmpty() {
+//     return this.top === null;
+//   }
+// }
+// class Queue {
+//   constructor() {
+//     this.front = null;
+//   }
+
+//   enqueue(value) {
+//     const newNode = new Node(value);
+//     if (this.isEmpty()) {
+//       this.front = newNode;
+//     } else {
+//       let current = this.front;
+//       while (current.next !== null) {
+//         current = current.next;
+//       }
+//       current.next = newNode;
+//     }
+//   }
+
+//   dequeue() {
+//     if (this.isEmpty()) {
+//       throw new Error('Queue is empty');
+//     }
+//     const value = this.front.value;
+//     this.front = this.front.next;
+//     return value;
+//   }
+
+//   peek() {
+//     if (this.isEmpty()) {
+//       throw new Error('Queue is empty');
+//     }
+//     return this.front.value;
+//   }
+
+//   isEmpty() {
+//     return this.front === null;
+//   }
+// }
+
+// module.exports = { Stack, Queue };
+
+
 class Node {
   constructor(value) {
     this.value = value;
     this.next = null;
   }
 }
+
 class Stack {
   constructor() {
     this.top = null;
@@ -35,35 +115,41 @@ class Stack {
     return this.top === null;
   }
 }
+
 class Queue {
   constructor() {
     this.front = null;
+    this.rear = null;
   }
 
   enqueue(value) {
     const newNode = new Node(value);
-    if (this.isEmpty()) {
+    if (!this.front) {
       this.front = newNode;
+      this.rear = newNode;
     } else {
-      let current = this.front;
-      while (current.next !== null) {
-        current = current.next;
-      }
-      current.next = newNode;
+      this.rear.next = newNode;
+      this.rear = newNode;
     }
   }
 
   dequeue() {
-    if (this.isEmpty()) {
+    if (!this.front) {
       throw new Error('Queue is empty');
     }
-    const value = this.front.value;
+
+    const dequeuedValue = this.front.value;
     this.front = this.front.next;
-    return value;
+
+    if (!this.front) {
+      this.rear = null; // If the queue becomes empty after dequeue
+    }
+
+    return dequeuedValue;
   }
 
   peek() {
-    if (this.isEmpty()) {
+    if (!this.front) {
       throw new Error('Queue is empty');
     }
     return this.front.value;
@@ -75,3 +161,4 @@ class Queue {
 }
 
 module.exports = { Stack, Queue };
+
